29235c3a79b23939c2966a1a29ab96d9a783d13f
Farhana Aktar <farhanaaktar@live.co.uk>
2025-02-27 18:27:34 +0000

    bacground added

--- zamans/src/App.jsx
+++ zamans/src/App.jsx
@@ -9,14 +9,23 @@ function App() {
   
 
   return (
-    <div className=" flex flex-col min-h-screen mt-3">
+    <div className=" flex flex-col min-h-screen">
       <div className='flex-grow'>
    <Call/>
-        <h1 className="flex justify-center text-6xl font-bold text-blue-900 p-4
+        <h1 className="flex justify-center text-6xl font-bold text-blue-900
         font-arsenal font-bold">Zamans Electrical Engineering Works</h1>
 <p className='text-center text-green-500 text-xl mb-8'>Services for Domestic and Commercial Premisses</p>
-<Services/>
 
+<div className="relative w-full">
+  <img src="/bg.png" className="w-full" />
+  <h1 className="absolute top-1/4 left-1/3 transform -translate-x-1/2 text-6xl font-bold text-gray-800 font-arsenal italic">
+    London Based
+  </h1>
+  <h1 className="absolute top-1/3 left-1/3 transform -translate-x-1/2 text-4xl mt-4 font-bold text-gray-800 font-arsenal italic">
+    Affordable Solutions
+  </h1>
+</div>
+<Services/>
 
 <Images/>
 
--- zamans/src/components/Images.jsx
+++ zamans/src/components/Images.jsx
@@ -2,10 +2,10 @@ const Images = () => {
     return (
       <div>
         {/* First Image and Text Block */}
-        <div className="flex justify-center items-center flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-12 mt-12">
+        <div className="flex justify-center items-center flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-12 mt-12 bg-gray-200">
           <img
             src="/1.jpg"
-            className="rounded-lg shadow-lg w-2/5 md:w-1/3 h-auto object-cover"
+            className="mt-2 mb-2 rounded-lg shadow-lg w-2/5 md:w-1/3 h-auto object-cover"
           />
           <div className="text-center md:text-left max-w-xl space-y-4">
             <h1 className="text-2xl text-gray-900 font-arsenal font-bold">
@@ -18,10 +18,10 @@ const Images = () => {
         </div>
   
         {/* Second Image and Text Block with Image on Right */}
-        <div className="flex justify-center items-center flex-col md:flex-row-reverse space-y-6 md:space-y-0 md:space-x-12 mt-12 mb-12">
+        <div className="flex justify-center items-center flex-col md:flex-row-reverse space-y-6 md:space-y-0 md:space-x-12 mb-12 bg-gray-200">
           <img
             src="/4.jpg"
-            className="rounded-lg shadow-lg w-2/5 md:w-1/3 h-auto object-cover"
+            className="mt-2 mb-2 rounded-lg shadow-lg w-2/5 md:w-1/3 h-auto object-cover"
           />
           <div className="text-center md:text-left max-w-xl space-y-4">
             <h1 className="text-2xl text-gray-900 font-arsenal font-bold">
